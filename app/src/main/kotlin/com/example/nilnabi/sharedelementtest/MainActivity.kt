1167a2d95354e64dc5453d74fdcdfa4d9d5439d0
nilnabi <nilnabi8923@gmail.com>
2016-06-09 03:21:16 +0900

    with binding

--- app/src/main/kotlin/com/example/nilnabi/sharedelementtest/MainActivity.kt
+++ app/src/main/kotlin/com/example/nilnabi/sharedelementtest/MainActivity.kt
@@ -1,46 +1,40 @@
 package com.example.nilnabi.sharedelementtest
 
 import android.content.Intent
+import android.databinding.DataBindingUtil
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
 import android.support.v4.app.ActivityOptionsCompat
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
-import android.view.View
-import android.widget.ImageView
+import com.example.nilnabi.sharedelementtest.databinding.ActivityMainBinding
 import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val toolbar = findViewById(R.id.toolbar) as Toolbar?
-        setSupportActionBar(toolbar)
 
-        val fab = findViewById(R.id.fab) as FloatingActionButton?
-        fab!!.setOnClickListener(View.OnClickListener { view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show() })
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         val url = "http://www.at-x.com/images/program/f6efdec7d161ac9cc86695319ed55137.jpg"
 
-        val image = findViewById(R.id.first_image) as ImageView
-        image.setOnClickListener {
+        binding.contentMain.firstImage.setOnClickListener {
             startActivity(
                     Intent().apply {
                         setClass(applicationContext, SecondActivity::class.java)
                     },
                     ActivityOptionsCompat.makeSceneTransitionAnimation(
                             this,
-                            image,
-                            image.transitionName
+                            binding.contentMain.firstImage,
+                            binding.contentMain.firstImage.transitionName
                     ).toBundle()
             )
         }
 
         Picasso.with(applicationContext)
                 .load(url)
-                .into(image)
+                .into(binding.contentMain.firstImage)
 
     }
 
--- app/src/main/kotlin/com/example/nilnabi/sharedelementtest/SecondActivity.kt
+++ app/src/main/kotlin/com/example/nilnabi/sharedelementtest/SecondActivity.kt
@@ -1,23 +1,26 @@
 package com.example.nilnabi.sharedelementtest
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import com.example.nilnabi.sharedelementtest.databinding.ActivitySecondBinding
 import com.squareup.picasso.Picasso
 
 class SecondActivity : AppCompatActivity() {
 
+    lateinit var binding : ActivitySecondBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
 
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_second)
 
         val url = "http://www.at-x.com/images/program/f6efdec7d161ac9cc86695319ed55137.jpg"
 
-        val image = findViewById(R.id.second_image) as ImageView
         Picasso.with(applicationContext)
                 .load(url)
                 .fit()
-                .into(image)
+                .into(binding.secondImage)
     }
 }
